69fcda3fe4613d734c1531a10e5ffd364ad53954
An Yu <1009227+anyu@users.noreply.github.com>
2023-11-15 14:41:00 -0500

    style

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('soundButton').addEventListener('click', function () {
   var audio = document.getElementById('sound');
   if (audio.paused) {
       audio.play();
-      this.innerText = 'Pause';
+      this.innerText = 'pause breathing';
   } else {
       audio.pause();
       this.innerText = 'breathe with Vader';
